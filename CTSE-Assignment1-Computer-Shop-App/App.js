39f11269b575843b7ff1c6f8d8222b5eb0fc9b42
dilshanlahiru <lahiru.kariyawasam123@gmail.com>
2023-03-25 16:37:49 +0530

    changes in routes - lahiru

--- CTSE-Assignment1-Computer-Shop-App/App.js
+++ CTSE-Assignment1-Computer-Shop-App/App.js
@@ -1,17 +1,35 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import PassData from "./screens/ImagePickerTwo";
+import AdminHome from "./screens/AdminHome";
+import ItemsShop from "./screens/ItemsShop";
+import AddItems from "./screens/AddItems";
+import ItemDetailPage from "./screens/ItemDetailPage";
+import EditItemsPage from "./screens/EditItemsPage";
+import HomeBlogPage from "./screens/NewsBlogs/HomeBlogPage";
+import AddBlogPage from "./screens/NewsBlogs/AddBlogPage";
+import DetailsBlogPage from "./screens/NewsBlogs/DetailsBlogPage";
+import EditBlogPage from "./screens/NewsBlogs/EditBlogPage";
 
-const Stack = createNativeStackNavigator();
-
-export default function App() {
+const stack = createStackNavigator();
+export default function app() {
   return (
     <View style={styles.container}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Shop" component={ItemsShop} />
-        </Stack.Navigator>
+        <stack.Navigator>
+          <stack.Screen name="AdminHome" component={AdminHome} />
+          <stack.Screen name="ItemsShop" component={ItemsShop} />
+          <stack.Screen name="AddItems" component={AddItems} />
+          <stack.Screen name="EditItemsPage" component={EditItemsPage} />
+          <stack.Screen name="ItemDetailPage" component={ItemDetailPage} />
+          <stack.Screen name="HomeBlogPage" component={HomeBlogPage} />
+          <stack.Screen name="AddBlogPage" component={AddBlogPage} />
+          <stack.Screen name="DetailsBlogPage" component={DetailsBlogPage} />
+          <stack.Screen name="EditBlogPage" component={EditBlogPage} />
+          <stack.Screen name="Home" component={PassData} />
+        </stack.Navigator>
       </NavigationContainer>
     </View>
   );
